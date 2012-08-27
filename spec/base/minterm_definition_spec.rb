00d97840434a406b1312489025c4885e223edde5
Maher Hawash <gmhawash@gmail.com>
2012-08-26 19:43:30 -0700

    Add minterm specification.

--- spec/base/minterm_definition_spec.rb
+++ spec/base/minterm_definition_spec.rb
@@ -6,7 +6,22 @@ describe MintermDefinition do
       subject {MintermDefinition.new({'radix' => 3, 'variables' => 10}) }
 
       it "returns a radix mask representing each digit" do
-        subject.radix_mask.should == '3333333333'
+        subject.radix_mask.should == 3333333333
+      end
+
+      it "returns the mask specified by specific bit location" do
+        subject.default = {'radix' => 3}
+        yaml = YAML.load <<-YAML
+        radix:
+          2: [3,4,5]
+        YAML
+        subject.radix = yaml['radix']
+        subject.radix_mask.should == 3333222333
+      end
+
+      it "raises an error if radix is not defined" do
+        subject.radix = nil
+        expect { subject.radix_mask }.to raise_error(ArgumentError) 
       end
     end
   end
